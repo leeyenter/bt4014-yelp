fe25e7e8dac3eabafa802bf2d6696ba9dbbed131
Yen Ter <leeyenter@gmail.com>
2019-04-11 12:35:13 +0800

    Working review search

---
+++ Load Data - YT.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import pyarrow.parquet as pq
+import pyarrow as pa
+
+print("Loading review.json")
+review = pd.read_json('data/review.json', lines=True, orient='columns')
+print("Loading business.json")
+business = pd.read_json('data/business.json', lines=True, orient='columns').set_index('business_id')
+business.drop(['attributes', 'hours'], axis=1, inplace=True)
+print("Loading user.json")
+user = pd.read_json('data/user.json', lines=True, orient='columns').set_index('user_id')
+print("Loading checkin.json")
+checkin = pd.read_json('data/checkin.json', lines=True, orient='columns').set_index('business_id')
+
+for num_reviews in [2000, 50000, 100000, 300000]:
+    print("Generating sample of", num_reviews, "reviews")
+    
+    review_sample = review.sample(num_reviews, random_state = 1)
+    
+    business_ids = review_sample[['business_id']].drop_duplicates().set_index('business_id')
+    user_ids = review_sample[['user_id']].drop_duplicates().set_index('user_id')
+    
+    business_sample = business_ids.join(business).reset_index()
+    user_sample = user_ids.join(user).reset_index()
+    checkin_sample = business_ids.join(checkin).reset_index()
+    
+    print("   - Saving to Parquet")
+
+    review_sample.to_parquet("data/"+str(num_reviews)+"_review.parquet")
+    business_sample.to_parquet("data/"+str(num_reviews)+"_business.parquet")
+    user_sample.to_parquet("data/"+str(num_reviews)+"_user.parquet")
+    checkin_sample.to_parquet("data/"+str(num_reviews)+"_checkin.parquet")
+    
+#    pq.write_to_dataset(pa.Table.from_pandas(review_sample), root_path='data/'+str(num_reviews)+"_review.parquet", partition_cols=['business_id'])
+#    pq.write_to_dataset(pa.Table.from_pandas(business_sample), root_path='data/'+str(num_reviews)+"_business.parquet", partition_cols=['business_id'])
+#    pq.write_to_dataset(pa.Table.from_pandas(user_sample), root_path='data/'+str(num_reviews)+"_user.parquet", partition_cols=['user_id'])
+#    pq.write_to_dataset(pa.Table.from_pandas(checkin_sample), root_path='data/'+str(num_reviews)+"_checkin.parquet", partition_cols=['business_id'])
---
+++ Review Processing - YT.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import spacy
+from tqdm import tqdm
+tqdm().pandas()
+nlp = spacy.load("en_core_web_lg")
+import re
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+analyser = SentimentIntensityAnalyzer()
+from spacy_cld import LanguageDetector
+language_detector = LanguageDetector()
+nlp.add_pipe(language_detector)
+
+accepted_pos = {'NOUN', 'PROPN', 'ADV', 'ADJ', 'VERB', 'PRON'}
+contain_pos = {'DET', 'PART', 'ADP', 'NUM'}
+after_cconj_pos = {'ADV', 'ADJ', 'VERB'}
+permitted_start_ends = {('NOUN', 'ADJ'), ('NOUN', 'VERB'), ('PROPN', 'ADJ'), 
+                        ('PROPN', 'VERB'), ('ADJ', 'NOUN'), ('ADJ', 'PROPN'), 
+                        ('ADV', 'VERB'), ('PROPN', 'NOUN'), ('NOUN', 'PROPN'), 
+                        ('NOUN', 'NOUN'), ('PROPN', 'PROPN')}
+
+def pull_phrases(doc):
+    phrases = []
+    start_index = 0 # should be inclusive
+    end_index = 0 # should be exclusive
+
+    while start_index < len(doc):
+        while start_index < len(doc) and doc[start_index].pos_ not in accepted_pos:
+            start_index+=1
+        end_index = start_index
+        while end_index < len(doc) and doc[end_index].pos_ in accepted_pos.union(contain_pos):
+            end_index += 1
+            if end_index < len(doc) - 1 and doc[end_index].pos_ == 'CCONJ' and doc[end_index+1].pos_ in after_cconj_pos:
+                end_index += 2 # include related terms after conjunctions
+        while doc[end_index-1].pos_ in contain_pos:
+            end_index -= 1
+
+        # add phrase
+        if start_index >= len(doc):
+            break
+        if True:#(doc[start_index].pos_, doc[end_index-1].pos_) in permitted_start_ends:
+            phrase = []
+            for token_index in range(start_index, end_index):
+                token_text = doc[token_index].lemma_
+                if token_text == '-PRON-':
+                    token_text = doc[token_index].text.lower()
+                phrase.append(token_text)
+            phrase = ' '.join(phrase)
+            if len(phrase) > 1:
+                phrases.append(nlp(phrase))
+
+        # Move start_index to end_index, to continue scanning
+        start_index = end_index
+    return phrases
+
+for business_name in ['Shake Shack', 'In-N-Out Burger', 'The Cheesecake Factory']:
+    print(business_name)
+    reviews = pd.read_pickle('data/'+business_name+'_spacy')
+    print("    Applying sentiment analysis")
+    reviews['sentiment'] = reviews.text.progress_apply(lambda x: analyser.polarity_scores(x)['compound'])
+    print("    Filtering non-english reviews")
+    reviews['is_en'] = reviews.spacy.progress_apply(lambda doc: len(doc._.languages) == 0 or doc._.languages[0] == 'en')
+    reviews = reviews[reviews.is_en]
+    reviews.drop('is_en', axis=1, inplace=True)
+    print("    Pulling phrases")
+    reviews['phrases'] = reviews.spacy.progress_apply(pull_phrases)
+    reviews = reviews[reviews.phrases.apply(lambda x: len(x) > 0)]
+    print("    Saving files (1/2)")
+    reviews.drop('spacy', axis=1).to_pickle("data/phrases_" + business_name + ".pkl")
+    print("    Saving files (2/2)")
+    reviews.to_pickle("data/phrases_" + business_name + "_spacy.pkl")
