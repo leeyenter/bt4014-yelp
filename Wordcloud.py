8bd1f663013d8c95f8d533563be6684682578740
changhongjie <changhongjie1@gmail.com>
2019-04-12 11:30:38 +0800

    Update Wordcloud.py

--- Wordcloud.py
+++ Wordcloud.py
@@ -1,7 +1,11 @@
 from wordcloud import Wordcloud
 
 def wordcloud_from_dataframe(df, wordcloud_params=None):
-    """Generates wordcloud figure from a Pandas dataframe."""
-    df['wordcloud_score'] = df['count'] * df['score']
-    mapping = dict(map(tuple, temp[['phrase', 'wordcloud_score']].values))
-    return WordCloud(**wordcloud_params).generate_from_frequencies(mapping)
+    try:
+        df['wordcloud_score'] = df['count'] * df['score']
+        mapping = dict(map(tuple, temp[['phrase', 'wordcloud_score']].values))
+        WordCloud(**wordcloud_params).generate_from_frequencies(mapping).to_file('wordcloud.jpg')
+    except:
+        print("Error in generating wordcloud.")
+        return False
+    return True
